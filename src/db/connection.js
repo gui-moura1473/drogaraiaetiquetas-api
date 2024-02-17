0fc46f1f60b62e858d7ca1c41e9457847db7d72e
Guilherme Moura <killecarter1473@hotmail.com>
2024-02-17 02:01:45 -0300

    bug fixes

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
+
+const DATABASE_PASSWORD = encodeURIComponent(process.env.MONGODB_PASSWORD);
 
 const connection = async () => {
-    await mongoose.connect(`mongodb+srv://killecarter1473:fdZSb9vs8YeOtm7J@drogaraia-api.5svtpit.mongodb.net/?retryWrites=true&w=majority`)
+    await mongoose.connect(`mongodb+srv://killecarter1473:${DATABASE_PASSWORD}@drogaraia-api.5svtpit.mongodb.net/?retryWrites=true&w=majority`)
 }
 
 connection().catch(err => console.log(err));
--- src/server.js
+++ src/server.js
@@ -1,5 +1,4 @@
 const app = require('./app');
-require('dotenv').config();
 
 app.listen(3000, () => {
     console.log(`Drogaraia API listening on port 3000`)
