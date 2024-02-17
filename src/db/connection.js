7be644d4634762514aac294126c77fde46ce59b5
Guilherme Moura <killecarter1473@hotmail.com>
2024-02-17 02:17:25 -0300

    Corrigidos erros

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,10 +1,8 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const DATABASE_PASSWORD = encodeURIComponent(process.env.MONGODB_PASSWORD);
-
 const connection = async () => {
-    await mongoose.connect(`mongodb+srv://killecarter1473:${DATABASE_PASSWORD}@drogaraia-api.5svtpit.mongodb.net/?retryWrites=true&w=majority`)
+    await mongoose.connect(process.env.MONGODB_URL)
 }
 
 connection().catch(err => console.log(err));
